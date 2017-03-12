7d196f0b37df1a06e583308bcb90bfd4cbb95b85
QianGuoqing <qian_guoqing@yeah.net>
2017-03-12 14:10:28 +0800

    add new notes

---
+++ 第三章-dom盒子模型实战和dom封装/第三节-dom盒子模型实战和dom库封装/课时61-深入拓展-单例模式小技巧/utils.js
@@ -0,0 +1,100 @@
+/**
+ * Created by qianguoqing on 17/3/12.
+ */
+// -> 使用惰性思想（JavaScript高阶编程技巧之一）来封装常用的方法库
+// 第一次在给utils赋值的时候，就已经把兼容处理好了，把最后的结果放在flag变量中，以后再每一个方法中
+    // 只要是IE6-8不兼容的，就不需要重新的检测，只需要使用flag的值即可
+var utils = (function () {
+    var flag = 'getComputedStyle' in window; // flag->这个变量不销毁，存储的是判断当前的浏览器是否兼容getComputedStyle,兼容的话是标准浏览器，如果flag=false，说明不兼容，说明当前浏览器是IE6-8
+    return {
+        listToArray: function (likeArray) {
+            var ary = [];
+            try {
+                ary = [].prototype.slice.call(likeArray);
+            } catch (e) {
+                for (var i = 0; i < likeArray.length; i++) {
+                    ary[ary.length] = likeArray[i];
+                }
+            }
+            return ary;
+        },
+
+        formatJSON: function (jsonStr) {
+            var jsonData = null;
+            try {
+                jsonData = JSON.parse(jsonStr);
+            } catch (e) {
+                jsonData = eval('(' + jsonData + ')');
+            }
+            return jsonData;
+        },
+
+        getCSS: function (curEle, attr) {
+            var val = null, reg = null;
+            if (flag) {
+                val = window.getComputedStyle(curEle, null)[attr];
+            } else {
+                if (attr === "opacity") {
+                    val = curEle.currentStyle["filter"];
+                    reg = /^alpha\(opacity=(\d+(?:\.\d+)?)\)$/;
+                    val = reg.test(val) ? reg.exec(val)[1] / 100 : 1;
+                } else {
+                    val = curEle.currentStyle[attr];
+                }
+            }
+            reg = /^(-?\d+(\.\d+)?)(px|pt|em|rem)?$/;
+            return reg.test(val) ? parseFloat(val) : val;
+        },
+
+        offset: function (curEle) {
+            var disLeft = curEle.offsetLeft, disTop = curEle.offsetTop, par = curEle.offsetParent;
+            while (par) {
+                if (navigator.userAgent.indexOf("MSIE 8") === -1) {
+                    disLeft += par.clientLeft;
+                    disTop += par.clientTop;
+                }
+                disLeft += par.offsetLeft;
+                disTop += par.offsetTop;
+                par = par.offsetParent;
+            }
+            return {left: disLeft, top: disTop};
+        },
+
+        win: function (attr, value) {
+            if (typeof value === 'undefined') {
+                return document.documentElement[attr] || document.body[attr];
+            }
+            document.documentElement[attr] = value;
+            document.body[attr] = value;
+        },
+
+        // -> children: 获取currentElement下所有的元素子节点，兼容所有浏览器
+        // 如果传递了tagName，可以在获取的集合中进行二次筛选，
+        // 把指定标签名的获取到
+        children: function (currentElement, tagName) {
+            var ary = [];
+            if (/MSIE (6|7|8)/i.test(navigator.userAgent)) {
+                var nodeList = currentElement.childNodes;
+                for (var i = 0; i < nodeList.length; i++) {
+                    var curNode = nodeList[i];
+                    curNode.nodeType === 1 ? ary[ary.length] = curNode : null;
+                }
+                nodeList = null;
+            } else {
+                ary = this.listToArray(currentElement.children);
+            }
+
+            if (typeof tagName === 'string') {
+                for (var k = 0; k < ary.length; k++) {
+                    var curEleNode = ary[k];
+                    if (curEleNode.nodeName.toLowerCase() !== tagName.toLowerCase()) {
+                        ary.slice(k, 1);
+                        k--;
+                    }
+                }
+            }
+
+            return ary;
+        }
+    }
+})();
