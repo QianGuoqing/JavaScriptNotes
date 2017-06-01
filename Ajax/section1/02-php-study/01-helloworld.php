31fd104261175203280075e11fbb009e6cb04d44
QianGuoqing <qian_guoqing@yeah.net>
2017-06-01 16:16:45 +0800

    add ajax

---
+++ Ajax/section1/02-php-study/01-helloworld.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>真高兴啊，今天买了iPhone <?php echo 1+2+3; ?>s </h1>
+</body>
+</html>
---
+++ Ajax/section1/03-get-post/doPost.php
@@ -0,0 +1,5 @@
+<?php
+
+$username = $_GET['username'];
+$gender = $_GET['gender'];
+
